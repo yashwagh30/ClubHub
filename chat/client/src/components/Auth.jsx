63db034c3e61d4d538cd437d6148b4a79ba423cd
kjswaruph <121663328+kjswaruph@users.noreply.github.com>
2023-10-19 20:12:34 +0530

    chat sd

--- chat/client/src/components/Auth.jsx
+++ chat/client/src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { username, password, phoneNumber, avatarURL } = form;
-    const URL = 'http://localhost:5000/auth';  
+    const URL = 'https://vit-clubhub-chat-production.up.railway.app/auth';  
     const { data:{ token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`,{
       username, password, fullName: form.fullName, phoneNumber, avatarURL,
     })
